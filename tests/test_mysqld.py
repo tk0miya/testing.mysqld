87a1f5fbf7d24c806e5fe78462ab75658f8f32ce
tk0miya <i.tkomiya@gmail.com>
2013-10-07 18:21:35 +0900

    Add testcases

--- tests/test_mysqld.py
+++ tests/test_mysqld.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
+import signal
 import unittest
 import test.mysqld
 from time import sleep
@@ -35,3 +37,59 @@ class TestMysqld(unittest.TestCase):
         self.assertIsNone(mysqld.pid)
         with self.assertRaises(OSError):
             os.kill(pid, 0)  # process is down
+
+    def test_with_mysql(self):
+        with test.mysqld.Mysqld(my_cnf={'skip-networking': None}) as mysqld:
+            self.assertIsNotNone(mysqld)
+
+            # connect to mysql
+            conn = pymysql.connect(**mysqld.dsn())
+            self.assertIsNotNone(conn)
+
+            pid = mysqld.pid
+            os.kill(pid, 0)  # process is alive
+
+        self.assertIsNone(mysqld.pid)
+        with self.assertRaises(OSError):
+            os.kill(pid, 0)  # process is down
+
+    def test_multiple_mysql(self):
+        mysqld1 = test.mysqld.Mysqld(my_cnf={'skip-networking': None})
+        mysqld2 = test.mysqld.Mysqld(my_cnf={'skip-networking': None})
+        self.assertNotEqual(mysqld1.pid, mysqld2.pid)
+
+        os.kill(mysqld1.pid, 0)  # process is alive
+        os.kill(mysqld2.pid, 0)  # process is alive
+
+    def test_mysqld_is_not_found(self):
+        try:
+            path = os.environ['PATH']
+            os.environ['PATH'] = '/usr/bin'
+
+            with self.assertRaises(RuntimeError):
+                test.mysqld.Mysqld(my_cnf={'skip-networking': None})
+        finally:
+            os.environ['PATH'] = path
+
+    def test_fork(self):
+        mysqld = test.mysqld.Mysqld(my_cnf={'skip-networking': None})
+        if os.fork() == 0:
+            del mysqld
+            os.kill(os.getpid(), signal.SIGTERM)  # exit tests FORCELY
+        else:
+            os.wait()
+            sleep(1)
+            self.assertTrue(mysqld.pid)
+            os.kill(mysqld.pid, 0)  # process is alive (delete mysqld obj in child does not effect)
+
+    def test_copy_data_from(self):
+        data_dir = os.path.join(os.path.dirname(__file__), 'copy-data-from')
+        mysqld = test.mysqld.Mysqld(my_cnf={'skip-networking': None},
+                                    copy_data_from=data_dir)
+
+        # connect to mysql
+        conn = pymysql.connect(**mysqld.dsn())
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM test.hello ORDER BY id')
+
+        self.assertEqual(cursor.fetchall(), [(1, 'hello'), (2, 'ciao')])
