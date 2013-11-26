af8d803e00c7bd5d34d75bd3948920441f42f7a7
tk0miya <i.tkomiya@gmail.com>
2013-11-26 10:57:55 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ classifiers = [
 
 setup(
     name='testing.mysqld',
-    version='1.1.1',
+    version='1.1.2',
     description='automatically setups a mysqld instance in a temporary directory, and destroys it after testing',
     long_description=open('README.rst').read(),
     classifiers=classifiers,
